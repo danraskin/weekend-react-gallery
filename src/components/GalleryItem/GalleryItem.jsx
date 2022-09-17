3eecd0ef2d924e375c29e1cb04deafce9bf0bdcd
Dan Raskin <Danieltraskin@gmail.com>
2022-09-17 12:43:36 -0500

    image toggle works

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
+
 export default function ({galleryItem, likeItem}) {
 
+let [ toggleStatus, setToggle ] = useState(true);
+//toggle output between:
+//     image ( <img>{item.path}</img> ) and
+//     description ( <p>{item.description}
 
-
+//click listener on itemCard child
+//toggle function
+const toggleCard = () => {
+    setToggle(!toggleStatus);
+}
 
     return (
         <div className="itemCard">
-            <form >
-                <img className="itemImg" src={galleryItem.path} />
+            <form className="displayField" onClick={e => toggleCard()}>
+                {toggleStatus ? <img className="itemImg" src={galleryItem.path} /> : <div>{galleryItem.description} </div>}
             </form>
             <input type="button" value="Like" onClick={e => likeItem(galleryItem.id)} />
             <p>🦑{galleryItem.likes}</p>
